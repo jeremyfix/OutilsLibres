7bf9e51de4eeb053dd0c9aecf828c52ae396fb18
jeremyfix <jeremy.fix@gmail.com>
2017-04-24 12:30:30 +0200

    add install script

---
+++ TP/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install lynx gawk
+
+#libopenjp2-7-dev
+
+# In xenial, I did not find the way to install the JP2
+# delegate Therefore, I use openjpeg-tools with j2k_to_image 
+
+sudo apt-get install openjpeg-tools mplayer vlc mpv
+
