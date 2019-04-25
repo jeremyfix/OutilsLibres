78f48a00e021be4827f3f0be99d74546dbe575e3
Jeremy Fix <jeremy.fix@gmail.com>
2019-04-25 12:43:11 +0200

    updating install script

--- TP/install.sh
+++ TP/install.sh
@@ -5,9 +5,9 @@ sudo apt install lynx gawk
 #libopenjp2-7-dev
 
 # In xenial, I did not find the way to install the JP2
-# delegate Therefore, I use openjpeg-tools with j2k_to_image
+# delegate Therefore, I use openjpeg-tools with opj_decompress
 
-sudo apt-get install openjpeg-tools mplayer vlc mpv
+sudo apt-get install mplayer vlc mpv libopenjp2-tools
 
 # For basemap,
 sudo apt install libgeos-3.6.2 libgeos-dev
