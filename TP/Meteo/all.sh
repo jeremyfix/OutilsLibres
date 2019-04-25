4247d1227cccaf323d86fab552467a080d40f65c
Jeremy Fix <jeremy.fix@gmail.com>
2019-04-25 20:54:45 +0200

    back to python 2. It works

--- TP/Meteo/all.sh
+++ TP/Meteo/all.sh
@@ -3,11 +3,12 @@
 # First, we clean up the directories
 ./clean.sh
 
-./scripts/meteo_get_date.sh | 
+./scripts/meteo_get_date.sh |
 ./scripts/meteo_generate_dates.py |
 ./scripts/meteo_get_data.sh |
 ./scripts/meteo_plot_temperatures.py |
 ./scripts/meteo_write_date.sh
 
 # We now generate the movie from all the images
-avconv -y -r 100 -i Images/%05d.png Videos/out.mp4 
+#avconv -y -r 100 -i Images/%05d.png Videos/out.mp4
+ffmpeg -y -r 100 -i Images/%05d.png Videos/out.mp4
--- TP/Meteo/scripts/meteo_plot_temperatures.py
+++ TP/Meteo/scripts/meteo_plot_temperatures.py
@@ -7,6 +7,7 @@ import os
 import matplotlib.pyplot as plt
 import datetime
 from mpl_toolkits.basemap import Basemap
+import scipy
 from scipy.interpolate import griddata
 import numpy as np
 import fileinput
@@ -23,17 +24,17 @@ while(True):
 
     # We receive the datafile from the input
     # which is something like : /path/to/ddMMYYYY.txt
-    
+
     datafile = line.rstrip('\n')
     filename = os.path.basename(datafile)
-    
+
     #print(datafile)
     #print(filename)
     date = datetime.datetime.strptime(filename.split('.')[0], "%d%m%Y")
 
     output_filename = "raw_images/%s.png" % date.strftime("%d%m%Y")
 
-    # Load the data 
+    # Load the data
     data = np.loadtxt(datafile)
 
     latitudes = data[:,1]
@@ -49,18 +50,18 @@ while(True):
     x, y = m(longitudes, latitudes)
     min_x, max_x = x.min(), x.max()
     min_y, max_y = y.min(), y.max()
-    
+
     # Interpolate the measures and plot them over the map
     N_samples = 200
 
     # Create a mesh on which to interpolate the datapoints
-    X_mesh, Y_mesh = np.meshgrid(np.linspace(min_x, max_x, N_samples), 
+    X_mesh, Y_mesh = np.meshgrid(np.linspace(min_x, max_x, N_samples),
                                  np.linspace(min_y, max_y, N_samples))
     points = np.zeros((data.shape[0],2))
     points[:,0] = x
     points[:,1] = y
     Z_mesh = griddata(points, values, (X_mesh, Y_mesh), method='linear')
-    
+
     # Remove the ocean data points
     #f = lambda x,y:not m.is_land(x,y)
     #test_not_is_land = np.vectorize(f)
@@ -74,7 +75,7 @@ while(True):
     cs = m.contourf(X_mesh, Y_mesh, Z_mesh, np.linspace(-40,40,21), alpha=0.7)
 
     #plt.title(date.strftime("%d %B %Y"))
-    
+
     from mpl_toolkits.axes_grid1 import make_axes_locatable
     divider = make_axes_locatable(plt.gca())
     cax = divider.append_axes("bottom", "5%", pad="3%")
