5ca09ad60cd9f1da80867f2c00cfe5e7283b6b14
Jeremy Fix <jeremy.fix@gmail.com>
2019-04-25 10:21:50 +0200

    updating to bionic

--- TP/install.sh
+++ TP/install.sh
@@ -5,11 +5,11 @@ sudo apt install lynx gawk
 #libopenjp2-7-dev
 
 # In xenial, I did not find the way to install the JP2
-# delegate Therefore, I use openjpeg-tools with j2k_to_image 
+# delegate Therefore, I use openjpeg-tools with j2k_to_image
 
 sudo apt-get install openjpeg-tools mplayer vlc mpv
 
 # For basemap,
-sudo apt install libgeos-3.5.0 libgeos-dev
-sudo pip install https://github.com/matplotlib/basemap/archive/master.zip
+sudo apt install libgeos-3.6.2 libgeos-dev
+sudo python -m pip install https://github.com/matplotlib/basemap/archive/master.zip
 
